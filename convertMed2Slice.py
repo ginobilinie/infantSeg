14ef3c0cf1daf2f09bae3dea9f48a185a9c831dc
no name <ginobilinie@users.noreply.github.com>
2017-03-06 00:24:13 -0500

    Add files via upload

--- convertMed2Slice.py
+++ convertMed2Slice.py
@@ -15,7 +15,7 @@ import numpy as np
 import scipy.io as scio
 from PIL import Image
 import scipy.misc as scmi
-
+from scipy import ndimage as nd
 
 
 path='/shenlab/lab_stor5/shzhou16/CuttedData192/planct1/'
@@ -23,6 +23,7 @@ outPath='/shenlab/lab_stor5/yzgao/planct1/'
 def main():
     #ids=[14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]
     ids=range(1,41)
+    rate=2.0/3
     
     for id in ids:
         gtfn=os.path.join(path,'P%d/V2Rct.nii.gz'%id)
@@ -32,6 +33,7 @@ def main():
         print 'mat shape, ', gtMat.shape
         for s in range(1,gtMat.shape[3]):
             sliceMat=gtMat[:,:,s]
+            sliceMatScale = nd.interpolation.zoom(sliceMat, zoom=rate)
             scmi.imsave('p%d_'%id+'s%d.png'%s, sliceMat)
         #gtMat=np.transpose(gtMat,(2,1,0))
 #         gtVol=sitk.GetImageFromArray(gtMat)
