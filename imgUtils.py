c70a0f781675327fb29ac1626d9f8343257b53b3
no name <ginobilinie@users.noreply.github.com>
2017-05-02 15:45:35 -0400

    Add files via upload

--- imgUtils.py
+++ imgUtils.py
@@ -29,6 +29,54 @@ def dice(im1, im2,tid):
     dsc=2. * intersection.sum() / (im1.sum() + im2.sum())
     return dsc
 
+class ScanFile(object):   
+    def __init__(self,directory,prefix=None,postfix=None):  
+        self.directory=directory  
+        self.prefix=prefix  
+        self.postfix=postfix  
+          
+    def scan_files(self):    
+        files_list=[]    
+            
+        for dirpath,dirnames,filenames in os.walk(self.directory):   
+            ''''' 
+            dirpath is a string, the path to the directory.   
+            dirnames is a list of the names of the subdirectories in dirpath (excluding '.' and '..'). 
+            filenames is a list of the names of the non-directory files in dirpath. 
+            '''  
+            for special_file in filenames:    
+                if self.postfix:    
+                    special_file.endswith(self.postfix)    
+                    files_list.append(os.path.join(dirpath,special_file))    
+                elif self.prefix:    
+                    special_file.startswith(self.prefix)  
+                    files_list.append(os.path.join(dirpath,special_file))    
+                else:    
+                    files_list.append(os.path.join(dirpath,special_file))    
+                                  
+        return files_list    
+      
+    def scan_subdir(self):  
+        subdir_list=[]  
+        for dirpath,dirnames,files in os.walk(self.directory):  
+            subdir_list.append(dirpath)  
+        return subdir_list  
+  
+if __name__=="__main__":  
+    dir=r"C:\os_path"  
+    scan=ScanFile(dir)  
+    subdirs=scan.scan_subdir()  
+    files=scan.scan_files()  
+      
+    print "The subdirs scaned are:"  
+    for subdir in subdirs:  
+        print subdir  
+      
+    print "The files scaned are:"  
+    for file in files:  
+        print file  
+
+
 def main():
     dataPath='D:\unc\bric\data\MRI_CT_histmatch_brain_prostate\fcn_bnRes\res_s16_v13/'
     ids=[1,2,3,4,5,6,7,8,9,10,11]
